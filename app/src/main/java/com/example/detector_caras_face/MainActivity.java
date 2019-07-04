4f667eeafe2784e1dacd2c60d632479617808e13
papileogithub123 <52531872+papileogithub123@users.noreply.github.com>
2019-07-04 09:17:26 -0300

    cambio1

--- app/src/main/java/com/example/detector_caras_face/MainActivity.java
+++ app/src/main/java/com/example/detector_caras_face/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(),"ssssssss",Toast.LENGTH_LONG).show();
 
-
+        Toast.makeText(getApplicationContext(),"ssssssss",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"ssssssss",Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(),"ssssssss",Toast.LENGTH_LONG).show();
 
       //  List<String> androids = List.of("Kitkat", "Lollipop", "Marshmallow");
